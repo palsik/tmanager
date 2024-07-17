3b9cbfd7283e5aab47a77f0982fe8c899b0d18d8
palsik <paulikmwendan@gmail.com>
2024-07-17 11:17:43 +0300

    Restored secret keys to prevent failure just in case

--- core/settings.py
+++ core/settings.py
@@ -21,10 +21,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY', default='palsik86')
 
-
-
-
+# OPENAI KEY: keep the secret key used in production secret!
+OPENAI_API_KEYS = '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'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
@@ -94,8 +95,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-
+# my secret key
+# SECRET_KEY = os.getenv("SECRET_KEY")
 if os.environ.get('DB_ENGINE') and os.environ.get('DB_ENGINE') == "postgreSQL":
     DATABASES = {
         'default': {
